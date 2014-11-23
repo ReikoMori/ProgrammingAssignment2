394b5fd387d0f1f1288c8f24db999f8c92dfa883
ReikoMori <reikomori1015@gmail.com>
2014-11-23 21:39:38 +0900

    2014/11/23 update to cachematrix.R by R.M.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A pair of functions that cache the inverse of a matrix - 11/24/2014 by R.M.
+## two functions: makeCacheMatrix, cacheSolve
 
-## Write a short comment describing this function
+## makeCacheMatrix : Function to create a special matrix object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL                               # Initialize the cache for inverse matrix
+        setMatrix <- function(y) {　　　        # setMatrix : 1st function to set matrix
+                x <<- y
+                i <<- NULL
+        }
+        getMatrix <- function() x               # getMatrix : 2nd function to get matrix 
+        setInverse <- function(inverse) i <<- inverse 
+        # setInverse : 3rd function to set inverse
+        getInverse <- function() i              # getInverse : 4th function to get inverse
+        list(setm = setMatrix, getm = getMatrix,     #list for the functions (names differ)
+             seti = setInverse,
+             geti = getInverse)
+        
 }
 
 
-## Write a short comment describing this function
+## cacheSolve : Function to to compute inverse for matrix object created by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i <- x$geti()                          # Get inverse cache and set to i
+        if(!is.null(i)) {                      # If i is not null, 
+                message("getting cached inverse matrix")    # write message,
+                return(i)                      # and return it.
+        }
+        message("setting cache with inverse matrix")
+        matrix <- x$getm()                     # If i is null, get matrix
+        i <- solve(matrix, ...)                #   set inverse of matrix
+        x$seti(i)                              #   and set it to i
+        i                                      # Print i 
 }
